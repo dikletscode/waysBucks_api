3a3378f2a2e440b1f6b91460e1ffbb96a033e40d
diky_herdian <dikyrain0@gmail.com>
2021-10-17 16:28:38 +0700

    origin development

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
   })
 );
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
   },
 });
 
