2f48d77d6b31d753504d562c263722716008937a
diky_herdian <dikyrain0@gmail.com>
2021-10-17 14:43:25 +0700

    seeders

--- seeders/20210831010326-user-seeder.js
+++ seeders/20210831010326-user-seeder.js
@@ -3,31 +3,35 @@ const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    let user = await queryInterface.bulkInsert(
-      "users",
-      [
+    try {
+      let user = await queryInterface.bulkInsert(
+        "users",
+        [
+          {
+            id: uuidv4(),
+            email: "admin@waysbuck.com",
+            role: 1,
+            password: await bcrypt.hash("admin123", 10),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        { returning: ["id"] }
+      );
+
+      await queryInterface.bulkInsert("profiles", [
         {
-          id: uuidv4(),
-          email: "admin@waysbuck.com",
-          role: 1,
-          password: await bcrypt.hash("admin123", 10),
+          userId: user[0].id,
+          fullname: "admin",
+          image:
+            "https://res.cloudinary.com/wuysback/image/upload/v1631446281/WaysBucks_1_vpwdlo.png",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
-      ],
-      { returning: ["id"] }
-    );
-
-    await queryInterface.bulkInsert("profiles", [
-      {
-        userId: user[0].id,
-        fullname: "admin",
-        image:
-          "https://res.cloudinary.com/wuysback/image/upload/v1631446281/WaysBucks_1_vpwdlo.png",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+      ]);
+    } catch (error) {
+      console.log(error);
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
