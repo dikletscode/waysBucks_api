f2e53b20cd6bb521ca599cbc7f6da2687dd2f7bb
diky_herdian <dikyrain0@gmail.com>
2021-10-17 14:12:31 +0700

    cors

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://waysbuck.netlify.app",
   })
 );
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://waysbuck.netlify.app",
   },
 });
 
